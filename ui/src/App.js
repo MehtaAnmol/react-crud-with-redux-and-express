9bdada4131a2d1165b5ca6428912a650d5463f03
Anmol Mehta <mehta.anmol48@gmail.com>
2021-03-12 20:14:01 +0530

    added list items component

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, BrowserRouter as Router, Redirect } from "react-router-dom";
+import List from "./Todo/List";
+import Header from "./Shared/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header></Header>
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/todo" />
+            </Route>
+            <Route exact path="/todo" component={List} />
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from "./reducers";
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(reducer);
+
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
